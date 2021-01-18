069411fa51d1f61432a479238068ffd771da517f
WikiGenius <muhammedelyamani92@gmail.com>
2021-01-18 23:19:03 +0200

    Add Section class

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -13,16 +13,64 @@
  * 
 */
 
+// Create section class 
+class Section {
+    constructor() {
+        // property to track last id
+        this.lastId = 0;
+        // capture main element once in this class
+        this.mainElement = document.querySelector("main");
+    }
+    get HTML_Content() {
+        // html content inside each section
+        let html_content = `<div class="landing__container">
+        <h2>Section ${this.lastId}</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra
+          dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus
+          imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget
+          bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet
+          elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo
+          nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie
+          semper in tellus. Sed congue et odio sed euismod.</p>
+
+        <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel
+          luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur
+          porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
+      </div>`;
+        return html_content;
+    }
+    addNewSection() {
+        // increment lastId for each calling
+        this.lastId += 1;
+        // grab the html content that fill in each section
+        let html_content = this.HTML_Content;
+        // Create section element
+        const new_secElem = document.createElement('section');
+        /* set some attributes */
+        // set id attribute
+        new_secElem.setAttribute('id', `section${this.lastId}`);
+        // set data attribute
+        new_secElem.setAttribute('data-nav', `Section ${this.lastId}`);
+        if (this.lastId === 1) {
+            // set class attribute
+            new_secElem.setAttribute('class', "your-active-class");
+        }
+        // Attach html content to the section
+        new_secElem.innerHTML = html_content;
+        // Append new section element to main element
+        this.mainElement.appendChild(new_secElem);
+    }
+}
 /**
  * Define Global Variables
- * 
+ *
 */
-
+section = new Section();
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
+ *
 */
 
 
@@ -30,7 +78,7 @@
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
 */
 
 // build the nav
@@ -45,7 +93,7 @@
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
 // Build menu 
@@ -55,3 +103,7 @@
 // Set sections as active
 
 
+section.addNewSection();
+section.addNewSection();
+section.addNewSection();
+section.addNewSection();
