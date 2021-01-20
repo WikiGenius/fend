02c7ec7eff2cc2d4a2a78c876a05002216cef22b
WikiGenius <muhammedelyamani92@gmail.com>
2021-01-20 03:20:55 +0200

    Set sections as active

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -14,6 +14,7 @@
 */
 
 // Create section class 
+start = performance.now();
 class Section {
     constructor() {
         // property to track last id
@@ -53,10 +54,7 @@ class Section {
         new_secElem.setAttribute('id', `section${this.lastId}`);
         // set data attribute
         new_secElem.setAttribute('data-nav', `Section ${this.lastId}`);
-        if (this.lastId === 1) {
-            // set class attribute
-            new_secElem.setAttribute('class', "your-active-class");
-        }
+
         // Attach html content to the section
         new_secElem.innerHTML = html_content;
         // Append new section element to main element
@@ -64,29 +62,20 @@ class Section {
     }
     create_NewNav() {
         // Create html content for navigation bar
-        let html_content = `<li>\
-        <a href="#section${this.lastId}" class="menu__link"\
-        data-section-id=section${this.lastId}>Section ${this.lastId}</a></li>`
+        let html_content = `<li>
+        <a href="#section${this.lastId}" class="menu__link"
+        data-section-id="section${this.lastId}"
+        id="nav${this.lastId}">Section ${this.lastId}</a></li>`
         // Create element
         this.list_menu.insertAdjacentHTML("beforeend", html_content);
     }
-    clickHandler_goToSection(event) {
-        event.preventDefault();
-        // grap section id from event target
-        let section_id = event.target.dataset.sectionId;
-        let section_elem = document.querySelector(`#${section_id}`);
-        section_elem.scrollIntoView({ behavior: 'smooth' });
-    }
+
     addNewSection() {
         // create new section
         this.createNewSection();
         // create new navigation button
         this.create_NewNav();
-        // Modify behavior Event to scrol smooth to section
-        this.list_menu.addEventListener('click', this.clickHandler_goToSection);
-
     }
-
 }
 
 /**
@@ -94,6 +83,8 @@ class Section {
  *
 */
 section = new Section();
+active_section_id = null;
+list_menu = document.querySelector("#navbar__list");
 /**
  * End Global Variables
  * Start Helper Functions
@@ -103,7 +94,41 @@ section = new Section();
 function init_sections(num_sections) {
     for (let i = 0; i < num_sections; i++) { section.addNewSection(); }
 }
+function activeSection() {
+    // loop over sections
+    mySections.forEach(element => {
+        let rect = element.getBoundingClientRect();
+        // check there if there is new active section
+        if (rect.top >= 0 && rect.top <= 200) {
+            /*this desired section*/
 
+            // the active section id
+            section_id = element.id;
+            // active the section 
+            document.querySelector(`#${section_id}`).classList.add('your-active-class');
+            // the active nav id
+            nav_id = `nav${section_id.slice(7)}`;
+
+            // active the nav 
+            document.querySelector(`#${nav_id}`).classList.add('active_nav_element');
+
+            if (active_section_id !== null) {
+                // initial last section id
+                last_section_id = active_section_id;
+                // initial last nav id
+                last_nav_id = `nav${last_section_id.slice(7)}`;
+                if (section_id !== last_section_id) {
+                    // deactive the last activated section 
+                    document.querySelector(`#${last_section_id}`).classList.remove('your-active-class');
+                    // deactive the last activated nav
+                    document.querySelector(`#${last_nav_id}`).classList.remove('active_nav_element');
+                }
+            }
+            // update the global active_section_id
+            active_section_id = section_id;
+        }
+    })
+}
 
 
 /**
@@ -129,7 +154,20 @@ function init_sections(num_sections) {
 // Add event listener to Add section button
 function clickHandler_button_addNewSection() {
     section.addNewSection();
+    // update mySections
+    mySections = document.querySelectorAll('section');
 }
+function clickHandler_goToSection(event) {
+    event.preventDefault();
+    // grap section id from event target
+    let section_id = event.target.dataset.sectionId;
+    let section_elem = document.querySelector(`#${section_id}`);
+    section_elem.scrollIntoView({ behavior: 'smooth' });
+    
+}
+// Modify behavior Event to scrol smooth to section
+list_menu.addEventListener('click', clickHandler_goToSection);
+document.addEventListener('scroll', activeSection);
 // Build menu 
 
 // Scroll to section on link click
@@ -138,3 +176,5 @@ function clickHandler_button_addNewSection() {
 
 // Intial number of sections
 init_sections(4);
+mySections = document.querySelectorAll('section');
+console.log(performance.now() - start);
